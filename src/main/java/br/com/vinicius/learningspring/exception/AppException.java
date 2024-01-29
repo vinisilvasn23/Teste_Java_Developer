842a663a54dd705af3824717352f1b0cf176323f
Vinicius <vini456nascimento@gmail.com>
2024-01-29 16:42:21 -0300

    feat: created app exception

---
+++ src/main/java/br/com/vinicius/learningspring/exception/AppException.java
@@ -0,0 +1,17 @@
+package br.com.vinicius.learningspring.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class AppException extends RuntimeException {
+
+    private final HttpStatus statusCode;
+
+    public AppException(String message, HttpStatus statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+
+    public HttpStatus getStatusCode() {
+        return statusCode;
+    }
+}
